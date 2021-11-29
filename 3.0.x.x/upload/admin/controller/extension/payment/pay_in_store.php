dd8317e2620715a736302b7077ae23f290cca4e3
condor2 <condor20_05@yahoo.it>
2021-11-29 15:55:26 +0200

    Forget to change extension with marketplace

--- 3.0.x.x/upload/admin/controller/extension/payment/pay_in_store.php
+++ 3.0.x.x/upload/admin/controller/extension/payment/pay_in_store.php
@@ -14,7 +14,7 @@ class ControllerExtensionPaymentPayInStore extends Controller {
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link('extension/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true));
 		}
 
 		if (isset($this->error['warning'])) {
